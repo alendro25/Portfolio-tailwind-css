623f8b355fa9c4cdad0d48e5b1b738888ab30270
alendro25 <alendro25@gmail.com>
2022-12-13 15:06:40 +0700

    Tambahkan sedikit pembaruan

--- src/js/script.js
+++ src/js/script.js
@@ -2,11 +2,16 @@
 window.onscroll = function () {
   const header = document.querySelector("header");
   const fixedNav = header.offsetTop;
+  const toTop = document.querySelector("#to-top");
 
   if (window.pageYOffset > fixedNav) {
     header.classList.add("navbar-fixed");
+    toTop.classList.remove("hidden");
+    toTop.classList.add("flex");
   } else {
     header.classList.remove("navbar-fixed");
+    toTop.classList.remove("flex");
+    toTop.classList.add("hidden");
   }
 };
 
@@ -14,8 +19,18 @@ window.onscroll = function () {
 const hamburger = document.querySelector("#hamburger");
 // Navbar Menu
 const navMenu = document.querySelector("nav");
+// Layer
+const layer = document.querySelector("#layer");
 
 hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("hamburger-active");
   navMenu.classList.toggle("hidden");
+  layer.classList.toggle("hidden");
+});
+
+// Klik diluar Hamburger
+layer.addEventListener("click", function () {
+  hamburger.classList.remove("hamburger-active");
+  navMenu.classList.add("hidden");
+  layer.classList.add("hidden");
 });
